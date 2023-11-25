2b8f798cfd7aeca3a3d38ec076282d81526cb621
Antoine Fabri <antoine.fabri@gmail.com>
2023-11-25 17:03:07 +0100

    Deal with devtools masking `?`

---
+++ R/zzz.R
@@ -0,0 +1,18 @@
+# deal with the conflicting `?` in the  devtools_shims env
+# follow up in https://github.com/r-lib/pkgload/issues/265
+
+.onAttach <- function(...) {
+  if (isNamespaceLoaded("pkgload")) {
+    attr(`?`, "original") <- getFromNamespace("shim_question", "pkgload")
+    assignInNamespace("shim_question", `?`, "pkgload")
+  }
+}
+
+.onDetach <- function(...) {
+  if (isNamespaceLoaded("pkgload")) {
+    original <- attr(getFromNamespace("shim_question", "pkgload"), "original")
+    if (!is.null(original)) {
+      assignInNamespace(original, `?`, "pkgload")
+    }
+  }
+}
